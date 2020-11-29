a7f0be2aa3ceb19f055c833901f1d0158a929219
dashchin <dchin@college.harvard.edu>
2020-11-29 13:34:51 -0500

    kanye

--- r file/app.R
+++ r file/app.R
@@ -1,80 +1,146 @@
 library(shiny)
 library(tidyverse) 
 library(shinythemes)
+library(spotifyr)
+library(shinyWidgets)
 
 sinatra <- read_rds("sinatra.rds") 
 ewf <- read_rds("ewf.rds") 
+kanye <- get_artist_audio_features('kanye west')
+
+Sys.setenv(SPOTIFY_CLIENT_ID = '8a8f9963e2b84ea485694d8a99919904')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '1f4676b66f6c49389bfb5093ebfa0dee')
+
+access_token <- get_spotify_access_token()
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    theme = shinytheme("superhero"),
-    navbarPage("About Games", 
+    theme = shinytheme("united"),
+    navbarPage("kanye omaRi west", 
                tabPanel("About", 
-                        h1("About the data"), 
-                        p("So far, I have collected a variety of data sets for my project. 
-The graphs on this app represent two of them. I made a Frank Sinatra plot using a data set that catalogued the most popular songs on holiday seasons. 
-I know, at least generally, that I may want to chart the popularity of songs or genres over time, so I thought this was a good start. 
-I narrowed down that tibble to Frank Sinatra holiday songs just as an excercise. 
-Test
-The Earth, Wind & Fire plot was made with a different data set that has information on more than 160,000 songs. I filtered it for every EWF Song 
-and tried to model their most active years with the release_data column and the summarise() function. These are not necessarily graphs I'll need for the 
-final project, but they are examples of what I can do with the data.")), 
+                        h1("Introduction"), 
+                        p("To say the least, Kanye Omari West is an interesting guy. An artist, producer, fashion designer, celebrity, and cultural icon, West has established himself as both a pop culture icon and a divisive national figure. 
+                           However, while some might consider West to be unpredictable, maybe there are some trends or patterns we can observe in his music and activity. For example, how does the music vary between West's albums? 
+                           Are there more commonalities between his hits than we might think? Beyond his music, what about his presidential run this year? Did it affect West's streaming or Twitter engagement? 
+                           What can this tell us about presidential runs and their effect on the businesses of political outsiders? 
+                           In this project, I seek out to confront all of these questions - not necessarily to draw a definitive answer - but to perhaps shed some light on the mystery and fascination behind West's career."), 
+                        h5("The source code for this Shiny App can be found at my GitHub", a("HERE", href="https://github.com/dashchin/spotify_data_test"))), 
                tabPanel("Analysis", 
-                        mainPanel(  
-                            titlePanel("Graphs"),
-                            br(),
-                            tabsetPanel(type = "tabs",
-                                        tabPanel("Sinatra", plotOutput("sinatra_plot")),
-                                        tabPanel("EWF",  plotOutput("ewf_plot"))
-                            )
-                            
-                            
-                            
-                            
-                        )        
-                        
-                        
-                        
-                        ))
-        # Show a plot of the generated distribution 
-    
-        
-    )  
+                        tabsetPanel(type = "tabs",
+                                    tabPanel("Album Analysis",   
+                                             sidebarPanel( 
+                                                 textInput(inputId = "entered_text",                      # a name for the value you choose here
+                                                           label = "Place your title text here:",         # a label above the text box
+                                                           value = "Example"),
+                                                 sliderInput(inputId = "selected_year",                   # a name for the value you choose here
+                                                             label = "Choose a year:",  # the label to display above the slider
+                                                             min = 1958, max = 2019, value = 1958, animate = TRUE)
+                                             ),          
+                                             mainPanel(),
+                                    ), 
+                                    tabPanel("Track Analysis ",
+                                             mainPanel()
+                                    ),   
+                                    tabPanel("Sample Analysis ",
+                                             mainPanel()
+                                    ),
+                                    tabPanel("Tempo Analysis ",
+                                             mainPanel()
+                                    ), 
+                                    tabPanel("Key analysis ", 
+                                             mainPanel(
+                                                 titlePanel("Kanye's Keys"),
+                                                 titlePanel("Kanye's Favorite Keys (Overall)"),
+                                                 br(),
+                                                 plotOutput("kanye_keys_plot"), 
+                                                 br(),
+                                                 shinyWidgets::sliderTextInput(inputId = "selected_album", 
+                                                                               label = "Pick an Album (Most Recent First):", 
+                                                                               choices = c(unique(kanye$album_name))
+                                                 ),  
+                                                 titlePanel("Kanye's Favorite Keys (Per Album)"),
+                                                 plotOutput("kanye_keys_plot2"), 
+                                                 titlePanel("Distribution of Kanye's Keys"), 
+                                                 plotOutput("kanye_keys_plot3")
+                                             )
+                                    ), 
+                                    tabPanel("Presidential Run ",   
+                                             mainPanel(  
+                                                 titlePanel("Graphs")
+                                             )
+                                    ),
+                                    tabPanel("Danceability",   
+                                             mainPanel(  
+                                                 titlePanel("Graphs"),
+                                                 br(),
+                                                 plotOutput("my_plot")
+                                             )
+                                    )
+                        )
+               )
+    )
+) 
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$sinatra_plot <- renderPlot({ 
-        sinatra %>% 
-            ggplot(aes(x = title, y = total)) + 
-            geom_col() + 
-            labs(title = "Frank Sinatra Holiday Songs in Top Charts Internationally", 
-                 x = "Song Title", 
-                 y = "Number of Countries Where Songs are in Top Charts") + 
-            theme_bw()
+    
+    # Kanye's Favorite Keys (Overall) 
+    
+    output$kanye_keys_plot <- renderPlot  ({    
+        kanye %>% 
+            count(key_mode, sort = TRUE) %>% 
+            head(20) %>%  
+            ggplot(aes(x = fct_reorder(key_mode, n), y = n)) + 
+            geom_col(fill = "orange") + 
+            labs(x = "Key", 
+                 y = "Total number of songs written in key", 
+                 title = "Kanye's Favorite Keys",
+                 subtitle = "What key will he use next?") + 
+            theme_bw() 
         
     }) 
     
-    output$ewf_plot <- renderPlot  ({ 
-        ewf %>% 
-            ggplot(aes(x = release_year, y = total)) + 
-            geom_col() + 
-            labs(title = "Number of Earth, Wind & Fire Songs per Year", 
-                 subtitle = "Do You Remember? The 21st Night of September?", 
-                 x = "Release Year", 
-                 y = "Total Number of Songs") + 
-            theme_bw()
-    })  
+    # Kanye's Favorite Keys (Per Album)
     
-    output$about <- renderText({"So far, I have collected a variety of data sets for my project. 
-The graphs on this app represent two of them. I made a Frank Sinatra plot using a data set that catalogued the most popular songs on holiday seasons. 
-I know, at least generally, that I may want to chart the popularity of songs or genres over time, so I thought this was a good start. 
-I narrowed down that tibble to Frank Sinatra holiday songs just as an excercise. 
-
-The Earth, Wind & Fire plot was made with a different data set that has information on more than 160,000 songs. I filtered it for every EWF Song 
-and tried to model their most active years with the release_data column and the summarise() function. These are not necessarily graphs I'll need for the 
-final project, but they are examples of what I can do with the data."})
+    output$kanye_keys_plot2 <- renderPlot  ({    
+        kanye %>% 
+            filter(album_name == input$selected_album) %>% 
+            count(key_mode, sort = TRUE) %>% 
+            head(20) %>%  
+            ggplot(aes(x = fct_reorder(key_mode, n), y = n)) + 
+            geom_col(fill = "orange") + 
+            labs(x = "Key", 
+                 y = "Total number of songs written in key", 
+                 title = "Kanye's Favorite Keys",
+                 subtitle = "What key will he use next?") + 
+            theme_bw() 
+        
+    }) 
+    
+    # Distribution of Kanye's Keys
+    
+    output$kanye_keys_plot3 <- renderPlot ({ 
+        kanye %>% 
+            distinct(track_name, album_name, album_release_date, key_mode, mode_name) %>%  
+            mutate(year = substr(album_release_date, 1, 4)) %>%  
+            select(mode_name, album_name, year, album_release_date) %>%  
+            group_by(mode_name, year, album_release_date) %>%  
+            summarize(total = n()) %>%  
+            arrange(year) %>%  
+            filter(mode_name == "minor") %>%  
+            group_by(year) %>%  
+            summarize(total = sum(total))%>%  
+            ggplot(aes(x = year, y = total)) + 
+            geom_point() + 
+            labs(title = "Minor Keys per Album Release Year", 
+                 x = "Year", 
+                 y = "Number of Songs in Minor") 
+        
+    })
+    
+    
+    
 } 
 
 # Run the application 
